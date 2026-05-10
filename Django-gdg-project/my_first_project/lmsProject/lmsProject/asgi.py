fbf4d24d9705a1c67adc6531a4f25a9f571e3abf
Your Name <your.email@example.com>
2026-05-10 21:39:43 +0300

    Add BASE_DIR and sys.path configuration to ASGI and WSGI files

--- Django-gdg-project/my_first_project/lmsProject/lmsProject/asgi.py
+++ Django-gdg-project/my_first_project/lmsProject/lmsProject/asgi.py
@@ -8,8 +8,13 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
 import os
+import sys
+from pathlib import Path
 
 from django.core.asgi import get_asgi_application
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.append(str(BASE_DIR))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lmsProject.settings')
 
